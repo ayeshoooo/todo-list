e2bfdcccddcd321d59911da444060f8d30d1ebea
Ayeshoooo <112780976+ayeshoooo@users.noreply.github.com>
2023-02-01 13:35:03 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,53 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let todo : string[] = [];
+let loop = true ;
+
+while (loop){
+    const answers :{
+        TODO : string,
+        addmore : boolean
+    } = await inquirer.prompt([
+        {
+            type : "input",
+            name : "TODO",
+            message : "What do you want to add in your todo?"
+        },
+        {
+            type : "confirm",
+            name : "addmore",
+            message : "What do you want to add in your todo?",
+            default : false
+         
+        }
+    ])
+    const {TODO,addmore} = answers;
+    console.log(answers);
+    loop = addmore
+    if(TODO){
+        todo.push(TODO)
+
+    }else{
+        console.log("Kindly add valid input")
+    }
+}
+console.log(todo);
+
+if (todo.length > 0){
+    console.log("Your todo list : /n")
+    todo.forEach(todo =>{
+        console.log(todo)
+
+    });
+}else{
+    console.log("No todos found")
+}
+
+
+
+
+
+
+
